553269dbe5d595e5e599fc025654208c63a7535d
Victor-Borges-Silva <victorborges975@gmail.com>
2024-08-23 13:47:29 -0300

    Atualização de recurso

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 locals {
-  vpc_id = "vpc-99580efd"
+  vpc_id    = "vpc-0708bc60b0f86d2d7"
+  subnet_id = "subnet-0b95c3402dc7a40bd"
 }
 
 module "ec2" {
@@ -9,13 +10,15 @@ module "ec2" {
   vpc_id                 = local.vpc_id
   iam_instance_profile   = module.iam.nome_role
   vpc_security_group_ids = [module.securityGroup.sg_id]
+  subnet_id              = local.subnet_id
+  #key_name               = "Teste_INF_561"
 }
 
-module "iam" {
-  source                = "git@github.com:victor-borges-CRM/modulo-iam.git?ref=v1.0.2"
-  nome_role_iam         = "PermiteSSMGenrenciarEC2"
-  nome_instance_profile = "test_perfil_ec2"
-}
+#module "iam" {
+#  source                = "git@github.com:victor-borges-CRM/modulo-iam.git?ref=v1.0.2"
+#  nome_role_iam         = "PermiteSSMGenrenciarEC2"
+#  nome_instance_profile = "test_perfil_ec2"
+#}
 
 module "securityGroup" {
   source              = "git@github.com:victor-borges-CRM/modulo-securityGroup.git?ref=v1.0.2"
